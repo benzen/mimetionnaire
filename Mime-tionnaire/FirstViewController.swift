276fa5682f7ce24fd9a4f19c7f441532fe69e74e
Benjamin Dreux <benjamin.dreux@code3.ca>
2014-10-10 09:03:28 -0400

    Initial Commit

---
+++ Mime-tionnaire/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Mime-tionnaire
+//
+//  Created by Benjamin Dreux on 2014-10-10.
+//  Copyright (c) 2014 Benjamin Dreux, Pascal Chouinard. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Mime-tionnaire/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Mime-tionnaire
+//
+//  Created by Benjamin Dreux on 2014-10-10.
+//  Copyright (c) 2014 Benjamin Dreux, Pascal Chouinard. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
