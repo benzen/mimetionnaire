3d56d3e1b290c2ffa01260c1dcb6c5db4853017b
Benjamin Dreux <benjamin.dreux@code3.ca>
2014-10-16 08:46:40 -0400

    search bar is hided by default and can be hiden again

--- Mime-tionnaire/ListerMimeController.swift
+++ Mime-tionnaire/ListerMimeController.swift
@@ -10,23 +10,34 @@ import UIKit
 import AVKit
 import AVFoundation
 
-class ListerMimeController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ListerMimeController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate{
     
     @IBOutlet
     var tableView: UITableView?
     
+    @IBOutlet
+    var searchBar: UISearchBar?
+    
+    
     let store: MimeStore = MimeStore()
-        
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+            self.tableView?.reloadData()
+    }
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        self.searchBar?.text = nil
+        self.tableView?.reloadData()
+        hideSearchBar()
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        println("count line called \(store.listMime().count)" )
-//        return store.listMime().count
-        return  store.listMime().count
+        return  store.listMime(searchBar?.text).count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell:UITableViewCell = self.tableView?.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
         
-        let model: MimeModel = self.store.listMime()[indexPath.row] as MimeModel
+        let model: MimeModel = self.store.listMime(searchBar?.text)[indexPath.row] as MimeModel
         cell.textLabel?.text = model.nom
         return cell
     }
@@ -38,7 +49,7 @@ class ListerMimeController: UIViewController, UITableViewDelegate, UITableViewDa
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
         if (segue.identifier == "playVideo") {
             let indexPath = tableView?.indexPathForSelectedRow()
-            let model = self.store.listMime()[indexPath!.row] as MimeModel
+            let model = self.store.listMime(searchBar?.text)[indexPath!.row] as MimeModel
             let url = NSURL(string: model.videoUrl)
             if let subVC = segue.destinationViewController as? AVPlayerViewController {
                 subVC.player = AVPlayer(URL: url)
@@ -53,7 +64,7 @@ class ListerMimeController: UIViewController, UITableViewDelegate, UITableViewDa
     func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!) {
         if (editingStyle == UITableViewCellEditingStyle.Delete) {
             // handle delete (by removing the data from your array and updating the tableview)
-            let model = store.listMime()[indexPath.row]
+            let model = store.listMime(searchBar?.text)[indexPath.row]
             store.delete(model)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         }
@@ -64,6 +75,23 @@ class ListerMimeController: UIViewController, UITableViewDelegate, UITableViewDa
         // Do any additional setup after loading the view, typically from a nib.
         self.tableView?.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         self.tableView?.reloadData()
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView?.setContentOffset(CGPoint(x:0, y:searchBar!.frame.height), animated: true)
+
+    }
+    
+    func hideSearchBar(){
+        UIView.animateWithDuration(0.4, animations: {
+            var newBounds = self.tableView?.bounds
+            newBounds!.origin.y += self.searchBar!.bounds.size.height
+            self.tableView?.bounds = newBounds!
+        })
+        
+    
+
     }
 
     override func didReceiveMemoryWarning() {
--- Mime-tionnaire/MimeStore.swift
+++ Mime-tionnaire/MimeStore.swift
@@ -16,7 +16,8 @@ class MimeStore{
     let createTable = "CREATE TABLE mime (nom text, videoUrl text);"
     let insert = "INSERT INTO mime (nom, videoUrl) VALUES (?, ?);"
     let list = "SELECT nom, videoUrl FROM mime;"
-    let delete = "delete from mime where nom=? and videoURL=?;"
+    let delete = "DELETE FROM mime where nom=? and videoURL=?;"
+    let find = "SELECT nom, videoUrl FROM mime WHERE nom like ?;";
     let db = SQLiteDB.sharedInstance()
     
     func addMime(mime: MimeModel){
@@ -26,8 +27,9 @@ class MimeStore{
         }
     }
     
-    func listMime() -> Array<MimeModel> {
-        let resultSet = db.query(list)
+    func listMime(query: String?) -> Array<MimeModel> {
+        let resultSet =  query!.isEmpty ? db.query(list) : db.query(find, parameters:["\(query!)%"])
+        
         return resultSet.map {
             (row:SQLRow) -> MimeModel in
             let nom = row["nom"]!.asString()
